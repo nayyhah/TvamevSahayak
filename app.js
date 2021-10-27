ec54f57cfc9a88be5fd7274547ab8cfbd628e324
Neha Jha <njha7189@gmail.com>
2021-10-27 11:47:27 +0530

    mail added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+const nodemailer = require('nodemailer');
 const port = process.env.PORT || 3000
 
 
@@ -17,7 +18,40 @@ res.set({
 	'Access-control-Allow-Origin': '*'
 	});
 return res.redirect('public/index.html');
-}).listen(process.env.PORT || 3000)
+})
 
+app.post('/joinus', (req,res)=>{
+    const {Name ,Email,Age,WhatsappNo,TimeCommitment,WhyDoYouWantToJoin,HowWillYouContributeToTheOrganisation  } = req.body;
+    
+    var transporter = nodemailer.createTransport({
+     service: 'gmail',
+     auth: {
+       user: 'tvamevsahayak@gmail.com',
+       pass: '09081965@'
+     }
+   });
+   
+   var mailOptions = {
+     from: 'njha7189@gmail.com',
+     to: Email,
+     subject: 'Sending Email using Node.js',
+     text: `Hii, I am ${Name}, ${Age} year old wanted to join TVAMEV SHAYAK. I will be working with full commitment to 
+                the organisation. I will work ${TimeCommitment} months. I want to join because ${WhyDoYouWantToJoin}.
+                I will contribute to organisation as ${HowWillYouContributeToTheOrganisation}.
+                WhatsApp No : ${WhatsappNo}  `
+   };
+   
+   transporter.sendMail(mailOptions, (error, info)=>{
+     if (error) {
+       console.log(error);
+     } else {
+       console.log('Email sent: ' + info.response);
+     }
+   });
+   res.redirect('/');
+   
+ })
 
-console.log(`Listening on port ${port}`);
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Listening on port ${port}`);
+})
